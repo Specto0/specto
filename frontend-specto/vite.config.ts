7d513f7e475996cd507f6c57090755d19691ebe4
Armando Daniel Teixeira Silva <8240559@estg.ipp.pt>
2025-12-05 02:03:58 +0000

    Erros de sintax

--- frontend-specto/vite.config.ts
+++ frontend-specto/vite.config.ts
@@ -1,21 +1,11 @@
-import path from "node:path";
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  server: {
-    proxy: {
-      '/auth': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-      },
+      "@": "/src",
     },
   },
 });
